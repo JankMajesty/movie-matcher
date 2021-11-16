7fa24c134e8d4310390392d8b510b5fb9d2d259f
JankMajesty <lmcgill36@gmail.com>
2021-11-16 17:04:38 -0600

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router'
+import {
+  TextField,
+  Button,
+  Container
+} from '@material-ui/core'
+
+class App extends Component {
+  state = {
+    username: '',
+    password: '',
+  }
+
+  handleTextChange = (e) => {
+    const state = { ...this.state }
+    state[e.target.name] = e.target.value
+    this.setState(state)
+  }
+
+  login = (e) => {
+    e.preventDefault()
+    document.cookie = "loggedIn=true"
+    window.location.replace("/")
+  }
+
+  render() {
+    if (this.state.redirectHome) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <div className="App">
+        <Container maxWidth="sm">
+          <form className="container" onSubmit={this.login}>
+            <TextField
+              required
+              onChange={this.handleTextChange}
+              value={this.state.username}
+              name="username"
+              label="Username"
+              type="text" />
+            <TextField
+              required
+              onChange={this.handleTextChange}
+              value={this.state.password}
+              name="password"
+              label="Password"
+              type="password" />
+            <Button
+              type="submit"
+              className="login-button"
+              variant="contained"
+              color="primary">Login</Button>
+          </form>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;

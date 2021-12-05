aac48849a826d352b51557feb3fc07554b221a6d
JankMajesty <lmcgill36@gmail.com>
2021-12-05 17:29:48 -0600

    removing build breaking component

--- src/Router.js
+++ src/Router.js
@@ -4,7 +4,7 @@ import cookie from 'cookie'
 import Login from './components/Login'
 import Home from './components/Home'
 import SignUp from './components/SignUp'
-import Favorites from './components/Favorites'
+
 
 
 export const checkAuth = () => {
@@ -12,25 +12,12 @@ export const checkAuth = () => {
     return cookies["loggedIn"] ? true : false;
   };
 
-  const ProtectedRoute = ({ component: Component, ...rest }) => {
-    return (
-      <Route
-        {...rest}
-        render={(props) =>
-          checkAuth() ? <Component {...props} /> 
-          : <Navigate to="/login" />
-        }
-      />
-    )
-  }
-
 const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/login" element={<Login/>} />
             <Route path="/signup" element={<SignUp/>} />
-            <Route path="/favorites" element={<Favorites/>} />
         </Routes>
     );
 };
